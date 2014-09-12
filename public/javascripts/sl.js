8975f927a637cc271c9a309d395e51f082e0ffa5
dkarabat <dkarabat13@gmail.com>
2014-09-12 17:52:16 +0300

    dfgfd

--- public/javascripts/sl.js
+++ public/javascripts/sl.js
@@ -1,28 +1,7 @@
- $(function() {
-      $('#slides').slidesjs({
-        alert('Hello')
-        width: 940,
-        height: 528,
-        navigation: false
-      });
-
-      /*
-        To have multiple slideshows on the same page
-        they just need to have separate IDs
-      */
-      $('#slides2').slidesjs({
-        width: 940,
-        height: 528,
-        navigation: false,
-        start: 3,
-        play: {
-          auto: true
-        }
-      });
-
-      $('#slides3').slidesjs({
-        width: 940,
-        height: 528,
-        navigation: false
-      });
+    $(function() {
+        $('#slides').slidesjs({
+            width: 940,
+            height: 528,
+            navigation: false
+        });
     });
