bfcf97f2e8e7098eaf3c29b43883b5a579200107
dkarabat <dkarabat13@gmail.com>
2014-09-12 18:28:51 +0300

    gdf

--- public/javascripts/sl.js
+++ public/javascripts/sl.js
@@ -1,6 +1,17 @@
- $('#slides').slidesjs({
-            width: 940,
-            height: 528,
-            navigation: false
- });
-
+$(function() {
+      $('#slides').slidesjs({
+        width: 940,
+        height: 528,
+        navigation: {
+          effect: "fade"
+        },
+        pagination: {
+          effect: "fade"
+        },
+        effect: {
+          fade: {
+            speed: 400
+          }
+        }
+      });
+    });
