5b4771687dcdd62a25a7e5fee7a643eef0a27da2
dkarabat <dkarabat13@gmail.com>
2014-11-07 15:05:15 +0200

    pachage fix chat

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var socket = io.connect('http://localhost:3000');
+    var socket = io.connect('/');
     var name = 'Гость_' + (Math.round(Math.random() * 100));
     var messages = $("#messages");
     var message_txt = $("#message_text")
